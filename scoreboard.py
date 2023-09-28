b27fb4e9516482aff9ce3f63df82074ce0e1f6a2
lrodriguesg <lrodrigues674@gmail.com>
2023-09-28 17:53:20 -0300

    add high score tracker

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,11 @@ FONT = ('Courier', 24, 'normal')
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.count_s = 0
+        self.score = 0
+        
+        with open('data.txt') as file:
+            self.hight_score = int(file.read())
+
         self.color('white')
         self.penup()
         self.setposition(0, 260)
@@ -14,16 +18,25 @@ class Scoreboard(Turtle):
         self.update_scoreboard()        
 
     def update_scoreboard(self):
-        self.write(f"score: {self.count_s}", align=ALIGNMENT, font=FONT)
-    
-    def game_over(self):
-        self.setposition(0, 0)
-        self.write("GAME OVER.", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"score: {self.score} | Hight Score: {self.hight_score}", align=ALIGNMENT, font=FONT)
+
+
+    def reset (self):
+        if self.score > self.hight_score:
+            self.hight_score = self.score
+            with open('data.txt', mode='w') as file_w:
+                file_w.write(str(self.hight_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.setposition(0, 0)
+    #     self.write("GAME OVER.", align=ALIGNMENT, font=FONT)
 
 
     def count_score(self):
-        self.count_s += 1
-        self.clear()
+        self.score += 1
         self.update_scoreboard()  
         
 
